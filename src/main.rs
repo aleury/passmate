fabbabd397e24b44a78a6c3c18afbc1bd513813f
Adam Eury <euryadam@gmail.com>
2024-09-13 20:27:56 -0400

    Swap directories crate for xdg. Remove windows from CI. (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::{Parser, Subcommand};
-use directories::ProjectDirs;
 use passmate::Vault;
 
 #[derive(Parser)]
@@ -16,18 +15,13 @@ enum Commands {
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-    let project_dirs =
-        ProjectDirs::from("", "", "passmate").expect("passmate directories not found");
+    let project_dirs = xdg::BaseDirectories::with_prefix("passmate")?;
     match args.command {
         Commands::Init => {
-            let config_dir = project_dirs.config_dir();
-            if !config_dir.exists() {
-                std::fs::create_dir_all(config_dir)?;
-            }
-            let vault_dir = config_dir.join("default.vault");
-            let vault = Vault::open(&vault_dir)?;
+            let path = project_dirs.place_config_file("default.vault")?;
+            let vault = Vault::open(&path)?;
             vault.save()?;
-            println!("Initialized vault at {}", vault_dir.display());
+            println!("Initialized vault at {}", path.display());
         }
     }
 
