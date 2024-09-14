63679ddd7cb30e5a06c847a02ea1a2c1569afb63
Adam Eury <euryadam@gmail.com>
2024-09-13 21:49:16 -0400

    Add a set command for adding a password to a vault. (#2)

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, fs::File, io::BufReader, path::PathBuf};
+
 use assert_cmd::Command;
 use tempfile::TempDir;
 
@@ -52,4 +54,33 @@ fn binary_with_init_command_creates_a_config_directory_for_the_applicaton() {
         )));
 
     assert!(vault_path.exists(), "expected vault to exist");
+}
+
+#[test]
+fn binary_with_set_command_adds_a_password_to_the_vault() {
+    let temp_config =
+        TempDir::with_prefix("config-").expect("failed to create temporary config directory");
+    Command::cargo_bin("passmate")
+        .unwrap()
+        .env(CONFIG_HOME, temp_config.path())
+        .arg("init")
+        .assert()
+        .success();
+
+    Command::cargo_bin("passmate")
+        .unwrap()
+        .env(CONFIG_HOME, temp_config.path())
+        .args(["set", "mypass", "test"])
+        .assert()
+        .success();
+
+    let want = HashMap::from([("mypass".into(), "test".into())]);
+    let got = read_vault_data_from_file(&temp_config.path().join("passmate/default.vault"));
+    assert_eq!(want, got);
+}
+
+fn read_vault_data_from_file(path: &PathBuf) -> HashMap<String, String> {
+    let file = File::open(path).unwrap();
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader).unwrap()
 }
